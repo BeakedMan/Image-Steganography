7873e895cdd288e205413e7aebe030aea97a38c8
BeakedMan <rishuravi.singh2021@vitstudent.ac.in>
2024-11-11 22:50:05 +0530

    For hosting changes made

--- app.py
+++ app.py
@@ -128,4 +128,6 @@ def extract():
     return render_template('extract.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+    # app.run(debug=True)
