889c1758d661b3ad57890be63357b28078955f54
BeakedMan <rishuravi.singh2021@vitstudent.ac.in>
2024-11-22 18:02:14 +0530

    Changes to app.py for deployment

--- app.py
+++ app.py
@@ -128,6 +128,7 @@ def extract():
     return render_template('extract.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    import os
+    port = int(os.environ.get('PORT', 8080))
+    app.run(debug=False, host='0.0.0.0', port=port)
     # app.run(debug=True)
